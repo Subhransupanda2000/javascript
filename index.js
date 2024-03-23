d7efd0825402b6b3629b6c60e253b9142cd87970
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2024-03-23 17:37:37 +0530

    counter program

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-var text="Helloworld"; 
-console.log("helloworld");
+
+let countlabel = document.getElementById("countlabel");
+let incresesbtn = document.getElementById("incresesbtn");
+let decresesbtn = document.getElementById("decresesbtn");
+let resetbtn = document.getElementById("resetbtn");
+
+
+let count = 0;
+incresesbtn.onclick = function () {
+    count++;
+    countlabel.textContent = count;
+
+}
+decresesbtn.onclick = function () {
+    count--;
+    countlabel.textContent = count;
+
+}
+resetbtn.onclick = function () {
+    count = 0;
+    countlabel.textContent = count;
+
+}
